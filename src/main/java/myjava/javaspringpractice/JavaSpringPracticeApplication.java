fede8b1989fa88136a62bea3e465767331480fd8
Karox1234 <zzzcomet@gmail.com>
2024-06-14 00:02:11 +0900

    First Commit

---
+++ src/main/java/myjava/javaspringpractice/JavaSpringPracticeApplication.java
@@ -0,0 +1,13 @@
+package myjava.javaspringpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/myjava/javaspringpractice/JavaSpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package myjava.javaspringpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
